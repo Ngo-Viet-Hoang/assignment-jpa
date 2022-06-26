795a517f3ed8c0243bbed19eb436c0831e6a56bb
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2022-06-26 19:19:51 +0700

    update

--- src/components/Street/CreateStreet.jsx
+++ src/components/Street/CreateStreet.jsx
@@ -45,16 +45,74 @@ class CreateStreet extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Hello CreateProduct</h1>
-             Name:   <input type="text" onChange={(ev) => this.setState({ name: ev.target.value }) }/> <br/>
-             Description:   <input type="text" onChange={(ev) => this.setState({ description: ev.target.value }) }/> <br/>
-             Status:   <select onChange={(ev) => this.setState({ status: ev.target.value }) }>
-                <option value={1}>Active</option>
-                <option value={2}>Deactive</option>
-             </select>
-             <button onClick={this.submitForm} type="sumit">Submit</button>
+            <>
+            <div className="container-scroller">
+                <Navbar />
+                <div className="container-fluid page-body-wrapper">
+                    <Slidebar />
+                    <div className="main-panel">
+                        <div className="content-wrapper">
+                            <div className="row">
+                                <div className="col-md-6 grid-margin stretch-card">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h4 className="card-title">Default form</h4>
+                                            <p className="card-description">Basic form layout</p>
+                                            <form className="forms-sample">
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputUsername1">Tên thành phố</label>
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        id="exampleInputUsername1"
+                                                        placeholder="Username"
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputEmail1">Tên Quận</label>
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        id="exampleInputEmail1"
+                                                        placeholder="Email"
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputPassword1">Ngày thành lập</label>
+                                                    <input
+                                                        type="number"
+                                                        className="form-control"
+                                                        id="exampleInputPassword1"
+                                                        placeholder="Password"
+                                                    />
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <div className="form-group row">
+                                                        <label className="col-sm-4 col-form-label">Status</label>
+                                                        <div className="col-sm-12">
+                                                            <select className="form-control">
+                                                                <option>using</option>
+                                                                <option>under construction</option>
+                                                                <option>under repair</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button type="submit" className="btn btn-primary me-2">
+                                                    Submit
+                                                </button>
+                                                <button className="btn btn-light">Cancel</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <Footer />
+                    </div>
+                </div>
             </div>
+        </>
         )
     }
 }
--- src/components/Street/StreetList.jsx
+++ src/components/Street/StreetList.jsx
@@ -1,66 +1,222 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-class StreetList extends Component {
-    constructor() {
-        super();
-        this.state = {
-           streets: [],
-        }
-    }
+import React, { useEffect, useState } from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import "./AirlineList.scss";
+import axios from "axios";
+import airlineService from "../../Shared/Services/AirlineService";
+import { Link, useLocation } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import DeleteAirline from "../DeleteAirline/DeleteAirline";
+import { DataGridPro } from "@mui/x-data-grid-pro";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from "@mui/material";
+import { BASE_URL_SERVER } from "../../../../../Configs/server";
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = () => {
-        fetch('https://assignment-jpa.herokuapp.com/api/v1/streets')
-            .then(res => res.json())
-            .then(res => {
-                this.setState({
-                    products: res
-                })
-            })
-    }
-
-    deleteProduct = (id) => {
-        const url = `https://spring-summer.herokuapp.com/api/v1/products/${id}`;
-        // fetch(url, {
-        //     method: 'DELETE',
-        //     headers: {
-        //         'Content-Type':'application/json',
-        //       },
-        //       body: JSON.stringify({})
-        // })
-        //     .then(res => res.json())
-        //     .then(res => {
-        //     })
-        //     this.fetchData();
-
-        axios.delete(url)
-        .then(res => {
-            this.fetchData();
-        });
-
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello ProductList</h1>
-                <ul>
-                    {
-                        this.state.streets.map((street, index) => {
-                            return (
-                                <li key={index}>{street.name} - Price: {street.description} 
-                                <button onClick={() => this.deleteProduct(street.id)}>Delete</button>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        )
-    }
+const columns = [
+  { id: "id", label: "Id", minWidth: 80 },
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "districtId", label: "DistrictId", minWidth: 100 },
+  {
+    id: "establishment",
+    label: "Establishment",
+    minWidth: 150,
+    align: "left",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: " description",
+    label: " Description",
+    minWidth: 100,
+    align: "left",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "status",
+    label: "Status",
+    minWidth: 80,
+    align: "left",
+  },
+];
+function createData(id, name, districtId, establishment, description, status) {
+  return { id, name, districtId, establishment, description, status };
 }
 
-export default StreetList;
+const rows = [
+  createData("1", "Vietnam Airline", "VN", "Viet Nam", "Bong sen vang"),
+  createData("2", "Bamboo Airways", "QH", "Viet Nam", "Cay tre"),
+  createData("3", "Vietravel Airlines", "VU", "Viet Nam", "Viettravel"),
+];
+
+export default function  StreetList() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [streetList, setStreetList] = useState([]);
+  const [select, setSelect] = React.useState([]);
+  const [msg, setMsg] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSelect(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  fetchData = () => {
+    fetch('https://assignment-jpa.herokuapp.com/api/v1/streets')
+        .then(res => res.json())
+        .then(res => {
+            this.setState({
+               streetList: res
+            })
+        })
+}
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+
+  return (
+    <>
+      <div id="airline">
+        <Paper sx={{ width: "100%" }}>
+          <Typography variant="h4" component="div" gutterBottom>
+            Street
+          </Typography>
+          <TableContainer sx={{ maxHeight: 5000 }}>
+            {msg !== "" ? (
+              <Stack sx={{ width: "100%" }} spacing={2}>
+                <Alert severity={msg.type}>{msg.content}</Alert>
+              </Stack>
+            ) : (
+              ""
+            )}
+
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" colSpan={3}>
+                    <Paper
+                      component="form"
+                      sx={{
+                        p: "2px 4px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: 250,
+                      }}
+                    >
+                      <IconButton sx={{ p: "10px" }} aria-label="menu">
+                        <MenuIcon />
+                      </IconButton>
+                      <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Search Airline"
+                        inputProps={{ "aria-label": "search google maps" }}
+                        value={searchValue}
+                        onChange={(ev) => setSearchValue(ev.target.value)}
+                      />
+                      <IconButton
+                        type="button"
+                        sx={{ p: "10px" }}
+                        aria-label="search"
+                      >
+                        <SearchIcon />
+                      </IconButton>
+                      <Divider
+                        sx={{ height: 28, m: 0.5 }}
+                        orientation="vertical"
+                      />
+                    </Paper>
+                  </TableCell>
+                 
+                  <TableCell align="right" colSpan={3}>
+                    <Link to={"/admin/airlines/create"}>
+                      <Button variant="contained" startIcon={<AddCircleIcon />}>
+                        Add New
+                      </Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ top: 100, minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {streetList
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((street) => {
+                    return (
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={street.Id}
+                      >
+                        <TableCell>{street.Id}</TableCell>
+                        <TableCell>{street.Name}</TableCell>
+                        <TableCell>{street.districtId}</TableCell>
+                        <TableCell>{street.establishment}</TableCell>
+                        <TableCell>{street.description}</TableCell>
+                        <TableCell>{street.status}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
+    </>
+  );
+}
